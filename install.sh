e81f687145d6dd7cc3deb3ddca49119835ae7a0c
hikalium <hikalium@hikalium.com>
2017-02-23 01:55:39 +0900

    Add .tmux.conf

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ cd `dirname $0`
 ln -snf ./dotfiles/.vimrc ../.vimrc
 ln -snf ./dotfiles/.bashrc ../.bashrc
 ln -snf ./dotfiles/.bash_profile ../.bash_profile
+ln -snf ./dotfiles/.tmux_conf ../.tmux_conf
