dec3f0082d252841a04319e0c1486e6e36c9e2c3
hikalium <hikalium@hikalium.com>
2017-03-04 19:47:59 +0900

    Fix

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 cd `dirname $0`
 
 pwd=`pwd`
-echo $pwd/.vim
 
 ln -snf $pwd/.vimrc ../.vimrc
 ln -snf $pwd/.bashrc ../.bashrc
 ln -snf $pwd/.bash_profile ../.bash_profile
 ln -snf $pwd/.tmux.conf ../.tmux.conf
+
+mkdir -p ../.vim/ftdetect
 ln -snf $pwd/.vim/ftdetect/binary.vim ../.vim/ftdetect/binary.vim
