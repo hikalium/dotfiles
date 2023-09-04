7ba757386c260bc2bce5110162cf58cccaa01446
hikalium <hikalium@hikalium.com>
2023-09-04 20:42:11 +0900

    update

--- scripts/setup_nvim_linux_postinstall.sh
+++ scripts/setup_nvim_linux_postinstall.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
 export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-#nvm install node
+
+nvm install node
 
 ~/bin/nvim.appimage -c ':PlugInstall'
 ~/bin/nvim.appimage -c ':CocInstall coc-rust-analyzer'
