38b2929af9828a29512c0d2861d5375079638903
hikalium <hikalium@hikalium.com>
2023-12-05 22:39:33 +0900

    update

---
+++ scripts/install_rust_analyzer.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.local/bin
+curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
