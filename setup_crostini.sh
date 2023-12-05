3534f4d45fc1f5912b6d66fb8f2ffe4d02311d3d
hikalium <hikalium@hikalium.com>
2023-12-05 22:43:30 +0900

    update

--- setup_crostini.sh
+++ setup_crostini.sh
@@ -7,7 +7,7 @@ else
 	git clone https://github.com/hikalium/dotfiles.git
 fi
 cd dotfiles
-git pull --no-rebase
+git pull --no-rebase https://github.com/hikalium/dotfiles.git
 sudo apt update
 sudo apt install -y fuse wget build-essential tmux minicom
 ./scripts/setup_git.sh
@@ -16,6 +16,7 @@ source ~/.bashrc || true
 ./link.sh
 ./scripts/setup_nvim_linux_postinstall.sh
 ./scripts/install_github_cli.sh
+./scripts/install_rust_analyzer.sh
 gh auth status || { gh auth login && gh auth status ; }
 git -C ~/dotfiles remote -v | grep https \
 	&& git -C ~/dotfiles remote set-url origin git@github.com:hikalium/dotfiles.git \
